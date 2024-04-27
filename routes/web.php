8a014b742b08c4f40ec2b19d727f8a916625d3a4
indromhars <indra.madaharsa@gmail.com>
2024-04-27 14:27:08 +0700

    feat: Adding daisyui and implementing view without controller

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/employee', function() {
+    return Inertia::render('Employee/Employee');
+})->name('employee');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
