125be7e81bfd47a200febfd75a054366fc6d70be
msikkc21 <izzaaida21@gmail.com>
2024-04-29 20:49:48 +0700

    Lt 1 Izza

--- resources/js/Pages/Latihan/Izza.jsx
+++ resources/js/Pages/Latihan/Izza.jsx
@@ -16,7 +16,7 @@ export default function Izza({ auth }) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">Izza</div>
+                        <div className="p-6 font-bold text-gray-900">SELAMAT DATANG!</div>
                     </div>
                 </div>
             </div>
