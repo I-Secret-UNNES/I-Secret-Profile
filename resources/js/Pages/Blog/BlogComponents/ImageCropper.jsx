0980f34048def25fc4382d4ff3c010c1d3fa84f0
rzkyalbani <balbani41@gmail.com>
2024-06-23 09:11:58 +0700

    test

---
+++ resources/js/Pages/Blog/BlogComponents/ImageCropper.jsx
@@ -0,0 +1,36 @@
+import React, { useRef } from 'react';
+import Cropper from 'react-cropper';
+import 'cropperjs/dist/cropper.css';
+
+const ImageCropper = ({ imageSrc, onCrop }) => {
+    const cropperRef = useRef(null);
+
+    const handleCrop = () => {
+        const cropper = cropperRef.current.cropper;
+        onCrop(cropper.getCroppedCanvas().toDataURL());
+    };
+
+    return (
+        <div>
+            <Cropper
+                src={imageSrc}
+                style={{ height: 400, width: '100%' }}
+                aspectRatio={16/9}
+                guides={false}
+                ref={cropperRef}
+                viewMode={1}
+                autoCropArea={1}
+                background={false}
+                responsive={true}
+                checkOrientation={false}
+            />
+            <div className='flex items-end justify-end'>
+                <button type='button' onClick={handleCrop} className='btn text-white mt-4'>
+                    Crop Image
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default ImageCropper;
